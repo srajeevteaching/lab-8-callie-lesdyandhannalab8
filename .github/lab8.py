8c8630ae338d23f383323bdc87a1b6ab0a7ebedc
Callie <ccwalker@loyola.edu>
2021-11-16 14:59:59 -0500

    lab8 - finished

--- .github/lab8.py
+++ .github/lab8.py
@@ -1,30 +1,42 @@
+#Programmers: Callie Walker, Lesdy Galvez, Hanna Magan
+#Date: 11/11/21
+#Lab Number: 8
+#
+#
 import random
 names_file = open("names.txt", "r")
 
 nameList = []
 def readLine(file,list):
     for line in file:
-        print(line)
         list.append(line)
+        #print(list)
 
 
 def getName(list):
     name = random.choice(list)
     list.remove(name)
+    #print(list)
     return name
 
 def main():
     readLine(names_file, nameList)
-    getName(nameList)
-    for i in nameList:
-        getName(nameList)
-        userInput = input("More names? ")
+    startName = "First name is "
+    randomName = random.choice(nameList)
+    nameList.remove(randomName)
+    print(randomName)
+    while len(nameList) > 0:
+        userInput = input("Pick another name? ")
         userInput.strip().lower()
-        print(getName(nameList))
-        if (userInput != "quit"):
-            getName(nameList)
-        if(len(nameList) == 0):
-            print("No more names available")
-    print(nameList)
+        if (userInput == "yes"):
+            print(getName(nameList))
+            if (len(nameList) == 0):
+                print("No more names available")
+                break
+        elif(userInput == "quit"):
+            print("Finished")
+            break
+    #print(nameList)
 main()
 names_file.close()
+#could creat while loop while length>0 and userInput == yes
